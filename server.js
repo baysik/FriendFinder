1df9befd76d62d9e2562a13097cc54f7572a2cf7
baysik <iambaysik@gmail.com>
2019-08-07 20:28:59 -0600

    initial files created

---
+++ server.js
@@ -0,0 +1,8 @@
+// Require dependencies
+const express = require('express');
+const path = require('path');
+
+// Set up express
+const app = express();
+// Set port
+const PORT = process.env.PORT || 3000;
