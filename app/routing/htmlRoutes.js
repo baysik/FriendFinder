d43e29e0b3a3517f5c0612af029feb5775318ea6
baysik <iambaysik@gmail.com>
2019-08-08 10:35:16 -0600

    added routing to htmlRoutes

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,14 @@
+// dependencies
+const path = require('path');
+
+// routing
+module.exports = function(app) {
+    // route to survey page
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+      });
+    // default to homepage if no routes are found
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+      });
+}
--- server.js
+++ server.js
@@ -6,3 +6,17 @@ const path = require('path');
 const app = express();
 // Set port
 const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// require routes
+require('./app/routing/htmlRoutes')(app);
+
+
+// listen to port
+app.listen(PORT, function() {
+    console.log(`app is listening to localhost:${PORT}`);
+  });
+  
