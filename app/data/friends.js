b3ec27351b8bdd24e81f05a4e1578d2c8b3698f9
baysik <iambaysik@gmail.com>
2019-08-13 09:49:20 -0600

    fixed modal picture

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,17 +1,17 @@
 // array of friends
 let friends = [
-    {
-        name: "Ahmed",
-        photo:
-            "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
-    },
-    {
-        name: "Todd",
-        photo:
-            "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjV7Mb-8P7jAhUHylQKHUBjAowQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.communitylandtrust.ca%2Fwp-content%2Fuploads%2F2015%2F10%2F%3FND&psig=AOvVaw15MR7OgI0urJ5joMTNuY7o&ust=1565752567314242",
-        scores: [3, 1, 4, 5, 1, 2, 3, 5, 3, 1]
-    }
+     {
+          name: "Ahmed",
+          photo:
+               "http://music.virginia.edu/sites/music.virginia.edu/files/styles/faculty_profile_image/public/default_images/person-placeholder_3.png?itok=dtpqDURH",
+          scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+     },
+     {
+          name: "Todd",
+          photo:
+               "http://music.virginia.edu/sites/music.virginia.edu/files/styles/faculty_profile_image/public/default_images/person-placeholder_3.png?itok=dtpqDURH",
+          scores: [3, 1, 4, 5, 1, 2, 3, 5, 3, 1]
+     }
 ];
 // export array
 module.exports = friends;
