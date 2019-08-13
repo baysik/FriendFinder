b6cfb67f1d7db506487178ed2525f07612c4c0ad
baysik <iambaysik@gmail.com>
2019-08-12 22:16:49 -0600

    added best match functionality

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -17,43 +17,52 @@ module.exports = function(app) {
 
         // get info after submit
         let userData = req.body;
+        var totalDifference;
         // get score of most recent application
-        let userScore = userData.scores;
+        // let userScore = userData.scores;
         // set empty array
-        let userScoreArr = [];
-
-        // convert strings to integer and push to userScoreArr
-        for (let i = 0; i < userScore.length; i++) {
-            console.log(parseInt(userScore[i]));
-            userScoreArr.push(parseInt(userScore[i]));
-        }
+        // let userScoreArr = [];
 
         // console.log(friends)
-        console.log(userData);
+        console.log("USER DATA", userData);
 
         // loop through all objects in friends.js and get their scores
         for (let i = 0; i < friends.length; i++) {
-            console.log(friends[i].scores);
-            let storedScoreArr = friends[i].scores;
-            let totalDifference = 0;
+            //   console.log("Loop through submission: ", friends[i].scores);
+            let storedScoreArr = friends[i];
+            totalDifference = 0;
             // subtract userScoreArr from storedScoreArr
             // let x = userScoreArr.map(function(item, index) {
             //     return item - storedScoreArr[index];
             // });
-            totalDifference +=
-                parseInt(userScoreArr) - parseInt(storedScoreArr);
-            console.log(Math.abs(totalDifference));
+            // convert strings to integer and push to userScoreArr
+            for (let j = 0; j < storedScoreArr.scores.length; j++) {
+                var currentScores = storedScoreArr.scores[j];
 
-            if (totalDifference <= bestMatch) {
-                bestMatch.name = userData.name;
-                bestMatch.photo = userData.photo;
-                bestMatch.totalDifference = totalDifference;
+                console.log("ALL CURRENT SCORES: ", currentScores);
+
+                var newFriend = userData.scores[j];
+
+                console.log("newFriend Scores", newFriend);
+                // userScoreArr.push(parseInt(userScore[j]));
+
+                totalDifference += Math.abs(
+                    parseInt(newFriend) - parseInt(currentScores)
+                );
+                console.log("=============== ", totalDifference);
+            }
+            if (totalDifference <= bestMatch.friendDifference) {
+                console.log("Total Difference: ", totalDifference);
+                bestMatch.name = storedScoreArr.name;
+                bestMatch.photo = storedScoreArr.photo;
+                bestMatch.friendDifference = totalDifference;
             }
             // set to an absolute value
             // let absoluteArray = x.map(Math.abs);
             // console.log(absoluteArray);
         }
-        friends.push(req.body);
-        res.json(true);
+        friends.push(userData);
+        res.json(bestMatch);
+        console.log("BEST MATCH", bestMatch);
     });
 };
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // Require dependencies
-const express = require('express');
-// const path = require('path');
+const express = require("express");
+const path = require("path");
 
 // Set up express
 const app = express();
@@ -8,17 +8,18 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+);
 app.use(express.json());
 
 // require routes
-require('./app/routing/apiRoutes')(app);
-require('./app/routing/htmlRoutes')(app);
-
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 // listen to port
-app.listen(PORT, function () {
-  console.log(`app is listening to http://localhost:${PORT}`);
+app.listen(PORT, function() {
+    console.log(`app is listening to http://localhost:${PORT}`);
 });
